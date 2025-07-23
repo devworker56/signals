65fa2c9d077b1a1c931362735b63e860e5342ee3
devworker56 <alainkavena@gmail.com>
2025-07-23 14:51:29 -0400

    fixing nav bar links

--- includes/header.php
+++ includes/header.php
@@ -1,15 +1,15 @@
 <ul class="navbar-nav ms-auto">
     <li class="nav-item">
-        <a class="nav-link <?= $current_page == 'index.php' ? 'active' : '' ?>" href="index.php">Home</a>
+        <a class="nav-link <?= basename($_SERVER['PHP_SELF']) == 'index.php' ? 'active' : '' ?>" href="index.php">Home</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link <?= $current_page == 'features.php' ? 'active' : '' ?>" href="features.php">Features</a>
+        <a class="nav-link <?= basename($_SERVER['PHP_SELF']) == 'features.php' ? 'active' : '' ?>" href="features.php">Features</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link <?= $current_page == 'pricing.php' ? 'active' : '' ?>" href="pricing.php">Pricing</a>
+        <a class="nav-link <?= basename($_SERVER['PHP_SELF']) == 'pricing.php' ? 'active' : '' ?>" href="pricing.php">Pricing</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link <?= $current_page == 'contact.php' ? 'active' : '' ?>" href="contact.php">Contact</a>
+        <a class="nav-link <?= basename($_SERVER['PHP_SELF']) == 'contact.php' ? 'active' : '' ?>" href="contact.php">Contact</a>
     </li>
     <li class="nav-item ms-lg-2 mt-2 mt-lg-0">
         <a href="pricing.php#signup" class="btn btn-primary">Get Started</a>
